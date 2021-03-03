5eea0e573b931c6d40d584791688d5896f4b2486
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-04 00:07:13 +0800

    Rewrite Fetch

--- php/submit.php
+++ php/submit.php
@@ -1,11 +1,11 @@
 <?php  
-    $pos_id = 10;
+    $pos_id = 90;
     if (isset($_POST['submit'])){
         if(!empty($_POST[$pos_id])){
             echo "<br>Voted: <br>";
-            while($pos_id < 40){
+            while($pos_id > 80){
                 echo "Position $pos_id: ", $_POST[$pos_id], "<br>";
-                $pos_id+=10;
+                $pos_id-=10;
             }
         }
         else{
