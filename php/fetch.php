75e1cb43393743b09416572c220a7a0e657b5e51
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-04 00:06:34 +0800

    Rewrite Fetch

--- php/fetch.php
+++ php/fetch.php
@@ -1,17 +1,18 @@
 <?php
+
+	// echo "Position: ". 
 	$pos_id = 90;
-	$sql = "SELECT * FROM candidate WHERE position_id = $pos_id";
-	$pos_result = $conn->query($sql);
+	$pos_result = $conn->query("SELECT 'name' FROM candidate_position WHERE position_id = $pos_id");
+	
+	$candidates = $conn->query("SELECT * FROM candidate WHERE position_id = $pos_id");
 	echo "<td><div class='Candidate'><input required type='radio' name='$pos_id' class='agree-term' value = 'None' checked = 'checked'/> Abstain </div></td>"; // create abstain
-	while($cand = $pos_result->fetch_assoc()) {
-		$stud = "SELECT * FROM student WHERE student_id = $cand[student_id]";
-		echo "stud id: " . $cand["student_id"]. " position" . $cand["position_id"]. " party" . $cand["party_name"]. "<br>";
+	while($cand = $candidates->fetch_assoc()) {
 		$can_nm = $cand["candidate_id"];
-		echo "<td><div class='Candidate'><input required type='radio' name='$pos_id' class='agree-term' value = '$can_nm'/> Candidate $can_nm </div></td>";
+		
+		$student = $conn->query("SELECT 'name' FROM student WHERE student_id = $cand[student_id]");
+		// echo $student["name"];
+		echo "<td><div class='Candidate'><input required type='radio' name='$pos_id' class='agree-term' value = '$can_nm'/> Candidate name: ". $can_nm . " Party: " . $cand["party_name"]. "</div></td>";
 	}
-
-
-
 
 	function testOutput($conn){ // for testing submit button
 		// $candidates = array(1, 1);
