699ccdefd64cd40a31413ba299912f7eb580914e
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-10 00:11:38 +0800

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -7,16 +7,12 @@
     if ($conn->connect_error) {
         die("Connection to database failed: ". $conn->connect_error);
     }
-
-    // $result = mysql_query("SELECT 'name' FROM student");
-    // $row_cnt = mysql_num_rows($result);
-    //procedural
-    // $result = mysqli_query($conn, "SELECT 'name' FROM student");
-    // $row_cnt = mysqli_num_rows($result);
-    // $row_cnt = $result->num_rows; 
-    //oop
-    // $student = $conn->query("SELECT 'name' FROM student");
-    // $row_cnt = $student->num_rows;
-
-	// echo "Row Count: ", $row_cnt;
+    // $dbhost = "localhost";
+    // $dbuser = "root";
+    // $dbpass = "";
+    // $db = "test";
+    // $conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+    // if ($conn->connect_error) {
+    //     die("Connection to database failed: ". $conn->connect_error);
+    // }
 ?>
