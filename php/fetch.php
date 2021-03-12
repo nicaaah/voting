43c37cbe6aee9b3547b0aaa8c5fbdea198c25fec
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-12 21:54:29 +0800

    Revert "Move Files to legacy"
    This reverts commit 7de6e75cd1b9a4bb8a7ad51c1dd53deaa67de431.

--- php/fetch.php
+++ php/fetch.php
@@ -1,40 +1,23 @@
 <?php
-    include '../php/connect.php';
     // fetch template
     $positions = $conn->query("SELECT * FROM candidate_position"); // get positions
-    
     while($poss = $positions->fetch_assoc()){   // loop through all positions
         // insert position div here
-        
-            echo' <div id="F-container">';
-            echo '<a href="#" id="F-position" style="float: left;">'.$poss["position_name"].'</a><hr>';
+            echo $poss["position_name"];
             $pos_id = $poss["position_id"];
             
             // insert candidate divs here
-            echo '<div class="candidate-box" style="order: 1;">';
-                echo '<td><div><label class="checkbox"><input type="radio" name="'.$poss["position_name"].'"  id="vote"><span class="checkmark"></span> Abstain </label></div></td>'; // display abstain choice
+                echo "<td><div class='Candidate'><input required type='radio' name='$pos_id' class='agree-term' value = 'None' checked = 'checked'/> Abstain </div></td>"; // display abstain choice
                 $candidates = $conn->query("SELECT * FROM candidate INNER JOIN student ON candidate.student_id = student.student_id WHERE position_id = $pos_id");  // join tables for student and candidate
                 while($cand = $candidates->fetch_assoc()) { // loop through all candidates in a given position
-                    $can_nm1 = $cand["candidate_id"];
-                    $can_nm2 = $cand["student_id"];
-                    echo '<div>
-                                   <label class="checkbox">
-                                       <input type="radio" name="'.$poss["position_name"].'" id="vote">
-                                       <span class="checkmark"></span>
-                                               <a href="#"><img src="#" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="Candidate" ></a>
-                                               <div class="candidate-info">';
+                    $can_nm1 = $cand["fname"];
+                    $can_nm2 = $cand["lname"];
+                    $can_id = $cand["candidate_id"];
             
                     // display candidate
-                    echo '<td><a href="#" id="F-CandidateName"><b> Candidate name:' . $can_nm1. " " . $can_nm2  . '</b></a><a href="#" id="F-Partylist"> Party: ' . $cand["party_name"]. '</a> </td>';
+                    echo "<td><div class='Candidate'><input required type='radio' name='$pos_id' class='agree-term' value = '$can_id'/> Candidate name: ". $can_nm1. " " . $can_nm2  . " Party: " . $cand["party_name"]. "</div></td>";
             // end of candidate div
-             echo'                              </div>
-                                    </label>
-                            </div>
-                 </div>';
         // end of position div
-            echo'    </div>
-   
-                   <div></div>';
         }
     }
 ?>
