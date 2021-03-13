9fb0f2b2917a168bd560b577e454c7e6c15997a8
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-13 20:29:54 +0800

    Renamed files and edited script variables

--- php/vtPreview.php
+++ php/vtPreview.php
@@ -1,4 +1,22 @@
-<div id="myModal" class="F-modal">
+<!DOCTYPE html>
+<html>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+      <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+      <link rel="stylesheet" type="text/css" href="css/styles.css">
+      <script src="js/bootstrap.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+    </head>
+
+  
+  <body>
+
+    <!-- Trigger/Open The Modal -->
+    <!-- <button id="vote-button">Submit</button> -->
+
+    <!-- The Modal -->
+    <div id="confirmation" class="F-modal">
     <!-- Modal content -->
     <div class="F-modal-content">
         <div class="F-modal-header">
@@ -37,5 +55,35 @@
             <button type="button" class="F-returnBTN"><span>Return to Voting</span></button>
             <button type="button" class="F-confirmBTN"><span>Confirm Submission</span></button>
         </div>
+      </div>
     </div>
-</div>
+
+    <script>
+     // Get the modal
+      var confirmModal = document.getElementById("confirmation");               //first modal popup
+
+      // Get the button that opens the modal
+      var confirmBtn = document.getElementById("vote-button");                   //submit button from Voting page(Rica)
+
+      // Get the <span> element that closes the modal
+      var cancelBtn = document.getElementsByClassName("F-returnBTN")[0];
+
+      // When the user clicks the button, open the modal 
+      confirmBtn.onclick = function() {
+      confirmModal.style.display = "block";
+      }
+
+      // When the user clicks on <span>, close the modal
+      cancelBtn.onclick = function() {
+      modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+    </script>
+  </body>
+</html>
