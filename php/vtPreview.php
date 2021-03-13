b08c5199df0d76cf6a6efa4c207d4fc4331d92dc
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-13 21:41:14 +0800

    Update vtPreview.php

--- php/vtPreview.php
+++ php/vtPreview.php
@@ -85,33 +85,7 @@
           modal.style.display = "none";
         }
       }
-      //////////////// receipt/////////////////////
-      // Get the modal
-      var modal = document.getElementById("receipt");
-
-      // Get the button that opens the modal
-      // var btn = document.getElementById("confirm");
-      var confirmBtn = document.getElementsByClassName("F-confirmBTN")[0];
-
-      // Get the <span> element that closes the modal
-      //var span = document.getElementsByClassName("close")[0];
-
-      // When the user clicks the button, open the modal 
-      confirmBtn.onclick = function() {
-      modal.style.display = "block";
-      }
-
-      // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
-      modal.style.display = "none";
-      }
-
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == modal) {
-          modal.style.display = "none";
-        }
-      }
+      
     </script>
   </body>
 </html>
