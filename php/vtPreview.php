6e9fbb7a0166b1764f268461d34c0a04c9b56566
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-13 20:38:27 +0800

    Update vtPreview.php

--- php/vtPreview.php
+++ php/vtPreview.php
@@ -59,6 +59,7 @@
     </div>
 
     <script>
+      //////////////// confirmation/////////////////////
      // Get the modal
       var confirmModal = document.getElementById("confirmation");               //first modal popup
 
@@ -75,6 +76,33 @@
 
       // When the user clicks on <span>, close the modal
       cancelBtn.onclick = function() {
+      modal.style.display = "none";
+      }
+
+      // When the user clicks anywhere outside of the modal, close it
+      window.onclick = function(event) {
+        if (event.target == modal) {
+          modal.style.display = "none";
+        }
+      }
+      //////////////// receipt/////////////////////
+      // Get the modal
+      var modal = document.getElementById("receipt");
+
+      // Get the button that opens the modal
+      // var btn = document.getElementById("confirm");
+      var confirmBtn = document.getElementsByClassName("F-confirmBTN")[0];
+
+      // Get the <span> element that closes the modal
+      //var span = document.getElementsByClassName("close")[0];
+
+      // When the user clicks the button, open the modal 
+      confirmBtn.onclick = function() {
+      modal.style.display = "block";
+      }
+
+      // When the user clicks on <span> (x), close the modal
+      span.onclick = function() {
       modal.style.display = "none";
       }
 
